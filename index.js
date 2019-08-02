abdf3a9fb952979c8576752968b83ec22951e388
David Y. Stephenson <david@davidystephenson.com>
2019-08-02 11:35:51 +0200

    Added auth

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
-const router = require('./image/router')
+const imageRouter = require('./image/router')
+const authRouter = require('./auth/router')
+const userRouter = require('./user/router')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
-app.use(router)
+app.use(imageRouter)
+app.use(authRouter)
+app.use(userRouter)
 
 const port = process.env.PORT || 4001
-app.listen(port, () => console.log(`Listening on :${port}`))
+app.listen(
+  port,
+  () => console.log(`Listening on :${port}`)
+)
