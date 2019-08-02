dcf868dbbf20af012fa7fd5f3ee671224b9c5111
David Y. Stephenson <david@davidystephenson.com>
2019-08-02 09:57:24 +0200

    Add model

---
+++ image/model.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize')
+const db = require('../db.js')
+
+const Image = db.define(
+  'image',
+  {
+    url: Sequelize.STRING,
+    title: Sequelize.STRING
+  }
+)
+
+module.exports = Image
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const db = require('./db')
+const image = require('./image/model')
 
 const app = express()
 const port = process.env.PORT || 4001
